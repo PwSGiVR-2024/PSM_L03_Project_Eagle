cce3c0e62a928611891410d535ecedbdc54085fa
Jakub <jankowskijakub59@gmail.com>
2025-06-19 22:01:00 +0200

    Basement door

---
+++ Assets/CuttersItem.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class CuttersItem : Item
+{
+    protected override void Awake()
+    {
+        id = "Cutters";
+        itemName = "No¿yce do metalu";
+    }
+
+    protected override void OnUse()
+    {
+    }
+}
